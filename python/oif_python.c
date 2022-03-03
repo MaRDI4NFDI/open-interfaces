ab4646e0fd9d0e4c5e098dd94f63f33adec7b50d
René Fritze <rene.fritze@wwu.de>
2022-03-03 10:46:57 +0100

    harmonize directory naming scheme

--- python/oif_python.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by rene on 18.02.22.
-//
-
-#include "oif_python.h"
--- python/oif_python.h
+++
@@ -1,8 +0,0 @@
-#ifndef OIF_EXPERIMENTS_OIF_PYTHON_H
-#define OIF_EXPERIMENTS_OIF_PYTHON_H
-
-int oif_init_lang();
-int oif_eval_expression(const char *str);
-void oif_deinit_lang();
-
-#endif // OIF_EXPERIMENTS_OIF_PYTHON_H
