143d278b1ae2819a8c321c468af1820469554fd4
René Fritze <rene.fritze@wwu.de>
2022-04-07 18:38:44 +0200

    emit not implemented messages from python connector

--- lang_python/oif_python.c
+++ lang_python/oif_python.c
@@ -1,9 +1,16 @@
 #include "oif_config.h"
 
 #include <oif_connector/oif_interface.h>
+#include <stdio.h>
 
-int oif_lang_init() { return OIF_OK; }
+int oif_lang_init() {
+  printf("Python connector not implemented");
+  return OIF_LOAD_ERROR;
+}
 
-int oif_lang_eval_expression(OIF_UNUSED const char *str) { return OIF_OK; }
+int oif_lang_eval_expression(OIF_UNUSED const char *str) {
+  printf("Python connector not implemented");
+  return OIF_SYMBOL_ERROR;
+}
 
-void oif_lang_deinit() {}
+void oif_lang_deinit() { printf("Python connector not implemented"); }
