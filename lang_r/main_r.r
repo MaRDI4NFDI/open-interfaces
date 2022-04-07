a5bacb5b18780e8749829560012a11be2f54ca9f
René Fritze <rene.fritze@wwu.de>
2022-04-07 18:59:41 +0200

    force hardcoed path in R script

--- lang_r/main_r.r
+++ lang_r/main_r.r
@@ -1,6 +1,5 @@
-#!/usr/bin/env Rscript
+#!/usr/bin/env -S Rscript --vanilla
 
-dyn.load("liboif_connector.so")
-.C("oif_connector_init", "python")
+dyn.load("/home/rene/projekte/uni/mardi/m2/m2-mixed-experiments/cmake-build-debug/oif_connector/liboif_connector.so")
 .C("oif_connector_eval_expression", "print(6*7)")
 .C("oif_connector_eval_expression")
