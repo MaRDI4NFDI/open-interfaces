f8ebd7e09f441d19b7075133cc2a80208b1ef4c8
René Fritze <rene.fritze@wwu.de>
2022-04-08 14:49:19 +0200

    usable R main script

--- lang_r/main_r.r
+++ lang_r/main_r.r
@@ -1,8 +1,15 @@
 #!/usr/bin/env -S Rscript --vanilla
 
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)!=2) {
+  lang = "julia"
+  expr = "print(6*7)"
+} else {
+  lang = args[1]
+  expr = args[2]
+}
+
 dyn.load("/home/rene/projekte/uni/mardi/m2/m2-mixed-experiments/cmake-build-debug/oif_connector/liboif_connector.so")
-lang = "julia"
-expression = "print(6*7)"
 .Call("oif_connector_init_r", lang)
-.Call("oif_connector_eval_expression_r", expression)
+.Call("oif_connector_eval_expression_r", expr)
 .Call("oif_connector_deinit")
