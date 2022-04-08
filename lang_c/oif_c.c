8594ec104fec564bd74a9da04dbc409364c73ae8
René Fritze <rene.fritze@wwu.de>
2022-04-08 15:12:25 +0200

    add not implemented note to c connector

--- lang_c/oif_c.c
+++ lang_c/oif_c.c
@@ -1,12 +1,16 @@
 #include "oif_config.h"
 
 #include <oif_connector/oif_interface.h>
+#include <stdio.h>
 
 int oif_lang_init() { return OIF_OK; }
 
-int oif_lang_eval_expression(__attribute__((unused)) const char *str) {
-
-  return OIF_OK;
+int oif_lang_eval_expression(const char *str) {
+  fprintf(
+      stderr,
+      "Error: Evaluating string expression '%s' not possible in C connector\n",
+      str);
+  return OIF_NOT_IMPLEMENTED;
 }
 
 void oif_lang_deinit() {}
