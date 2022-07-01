18bf5c93936b552f30afa6b7c46cc516d0b49b8d
René Fritze <rene.fritze@wwu.de>
2022-07-01 16:24:51 +0200

    start a cg imp in cpp

--- lang_cpp/main_cpp.cc
+++ lang_cpp/main_cpp.cc
@@ -2,6 +2,7 @@ extern "C" {
 #include <oif_connector/oif_connector.h>
 }
 
+#include "oif_constants.h"
 #include <string>
 
 int main(int argc, char *argv[]) {
@@ -14,6 +15,15 @@ int main(int argc, char *argv[]) {
 
   oif_connector_init(lang.c_str());
   oif_connector_eval_expression(expr.c_str());
+
+  constexpr int N{2};
+  double A[N * N]{1, 0, 1, 0};
+  double b[N]{1, 1};
+  double x[N];
+
+  if (oif_connector_solve(N, A, b, x) != OIF_OK) {
+    return -1;
+  }
 
   oif_connector_deinit();
 }
--- lang_cpp/oif_cpp.cc
+++ lang_cpp/oif_cpp.cc
@@ -4,7 +4,9 @@ extern "C" {
 #include "oif_connector/oif_constants.h"
 #include <oif_connector/oif_interface.h>
 }
+#include <array>
 #include <iostream>
+#include <vector>
 
 int oif_lang_init() { return OIF_OK; }
 
@@ -17,9 +19,28 @@ int oif_lang_eval_expression(const char *str) {
 void oif_lang_deinit() {}
 
 int oif_lang_solve(int N, double *A, double *b, double *x) {
-  (void)N;
-  (void)A;
-  (void)b;
-  (void)x;
-  return OIF_RUNTIME_ERROR;
+  //! adjusted from https://cplusplus.com/forum/general/222617/
+  const double tol = 1.0e-6;
+
+  std::vector<double> R(b, b + N);
+  auto P = R;
+  int k = 0;
+
+  while (k < N) {
+    auto Rold = R; // Store previous residual
+    vec AP = matrixTimesVector(A, P);
+
+    double alpha = innerProduct(R, R) / max(innerProduct(P, AP), NEARZERO);
+    x = vectorCombination(1.0, x, alpha, P);   // Next estimate of solution
+    R = vectorCombination(1.0, R, -alpha, AP); // Residual
+
+    if (vectorNorm(R) < TOLERANCE)
+      break; // Convergence test
+
+    double beta = innerProduct(R, R) / max(innerProduct(Rold, Rold), NEARZERO);
+    P = vectorCombination(1.0, R, beta, P); // Next gradient
+    k++;
+  }
+
+  return x;
 }
