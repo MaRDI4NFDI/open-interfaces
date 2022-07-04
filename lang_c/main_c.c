a5ad6e0cd17300677c2d1fd355d04aae4403a5f4
René Fritze <rene.fritze@wwu.de>
2022-07-04 12:29:49 +0200

    [c/cpp] print solutions

--- lang_c/main_c.c
+++ lang_c/main_c.c
@@ -1,7 +1,10 @@
 #include <oif_connector/oif_connector.h>
 
 #include "oif_constants.h"
+#include <stdio.h>
 #include <string.h>
+
+void print_vector(const int N, const double *x);
 
 int main(int argc, char *argv[]) {
   char *lang = "julia";
@@ -20,7 +23,17 @@ int main(int argc, char *argv[]) {
   double x[2];
 
   if (oif_connector_solve(N, A, b, x) != OIF_OK) {
+    print_vector(N, x);
     return -1;
   }
+  print_vector(N, x);
   oif_connector_deinit();
+}
+
+void print_vector(const int N, const double *x) {
+  printf("Solution: [");
+  for (int i = 0; i < N; ++i) {
+    printf("%f,", x[i]);
+  }
+  printf("]\n");
 }
