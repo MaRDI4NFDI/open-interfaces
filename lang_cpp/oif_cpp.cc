a25048782ef201ea1eb41eeb1e8cf03afe307d76
René Fritze <rene.fritze@wwu.de>
2022-07-04 10:54:31 +0200

    complete the cpp CG

--- lang_cpp/oif_cpp.cc
+++ lang_cpp/oif_cpp.cc
@@ -5,8 +5,12 @@ extern "C" {
 #include <oif_connector/oif_interface.h>
 }
 #include <array>
+#include <cassert>
 #include <iostream>
+#include <limits>
 #include <vector>
+
+#include <cblas.h>
 
 int oif_lang_init() { return OIF_OK; }
 
@@ -20,27 +24,41 @@ void oif_lang_deinit() {}
 
 int oif_lang_solve(int N, double *A, double *b, double *x) {
   //! adjusted from https://cplusplus.com/forum/general/222617/
-  const double tol = 1.0e-6;
+  using namespace std;
+  constexpr double max_residual = 1.0e-6;
+  constexpr double eps = numeric_limits<double>::epsilon();
 
-  std::vector<double> R(b, b + N);
+  vector<double> R(b, b + N);
+  assert(long(R.size()) == long(N));
+  vector<double> AP(R.size(), 0.0);
   auto P = R;
   int k = 0;
 
   while (k < N) {
     auto Rold = R; // Store previous residual
-    vec AP = matrixTimesVector(A, P);
 
-    double alpha = innerProduct(R, R) / max(innerProduct(P, AP), NEARZERO);
-    x = vectorCombination(1.0, x, alpha, P);   // Next estimate of solution
-    R = vectorCombination(1.0, R, -alpha, AP); // Residual
+    cblas_dscal(N, 0.0, AP.data(), 1);
+    cblas_dgemv(CblasRowMajor, CblasNoTrans, N, N, 1.0, A, N, P.data(), 1, 0.0,
+                AP.data(), 1);
 
-    if (vectorNorm(R) < TOLERANCE)
-      break; // Convergence test
+    double alpha = cblas_ddot(N, R.data(), 1, R.data(), 1) /
+                   max(cblas_ddot(N, R.data(), 1, AP.data(), 1), eps);
 
-    double beta = innerProduct(R, R) / max(innerProduct(Rold, Rold), NEARZERO);
-    P = vectorCombination(1.0, R, beta, P); // Next gradient
+    // Next estimate of solution
+    cblas_daxpy(N, alpha, P.data(), 1, x, 1);
+    // Residual
+    cblas_daxpy(N, -alpha, AP.data(), 1, R.data(), 1);
+
+    if (cblas_dnrm2(N, R.data(), 1) < max_residual)
+      return OIF_OK;
+
+    double beta = cblas_ddot(N, R.data(), 1, R.data(), 1) /
+                  max(cblas_ddot(N, Rold.data(), 1, Rold.data(), 1), eps);
+    // Next gradient
+    cblas_dscal(N, beta, P.data(), 1);
+    cblas_daxpy(N, 1.0, R.data(), 1, P.data(), 1);
     k++;
   }
 
-  return x;
+  return OIF_RUNTIME_ERROR;
 }
