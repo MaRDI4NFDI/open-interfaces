a5ef5b0698708c7bcef0109fd78a38a66ab83128
René Fritze <rene.fritze@wwu.de>
2022-07-08 17:21:22 +0200

    call intol solve from julia

--- lang_julia/main_julia.jl
+++ lang_julia/main_julia.jl
@@ -13,5 +13,17 @@ if ok != 0
     println("Cannot load connector")
     ccall(:jl_exit, Cvoid, (Int32,), ok)
 end
+
 @ccall oif.oif_connector_eval_expression(expression::Cstring)::Cint
+
+const N = 2
+A = [2., 0., 1., 0.];
+b = [1., 1.]
+x = [0.,0.]
+ok = @ccall oif.oif_connector_solve(N::Cint, A::Ptr{Cdouble}, b::Ptr{Cdouble}, x::Ptr{Cdouble})::Cint
+if ok != 0
+    println("Cannot load connector")
+    ccall(:jl_exit, Cvoid, (Int32,), ok)
+end
+
 @ccall oif.oif_connector_deinit()::Cvoid
