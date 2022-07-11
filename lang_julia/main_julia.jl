401763169658f5f52a28e90195d998deedfb2d6f
René Fritze <rene.fritze@wwu.de>
2022-07-11 09:26:10 +0200

    refactor julia script

--- lang_julia/main_julia.jl
+++ lang_julia/main_julia.jl
@@ -1,5 +1,12 @@
 #!/usr/bin/env julia
 
+function check_call(result, msg="Cannot load connector")
+    if result != 0
+        println(msg)
+        ccall(:jl_exit, Cvoid, (Int32,), ok)
+    end
+end
+
 if size(ARGS)[1] == 2
     const lang = ARGS[1]
     const expression = ARGS[2]
@@ -8,22 +15,17 @@ else
     const expression = "print(7*6)"
 end
 const oif = "liboif_connector"
-ok = @ccall oif.oif_connector_init(lang::Cstring)::Cint
-if ok != 0
-    println("Cannot load connector")
-    ccall(:jl_exit, Cvoid, (Int32,), ok)
-end
+ret = @ccall oif.oif_connector_init(lang::Cstring)::Cint
+check_call(ret)
 
-@ccall oif.oif_connector_eval_expression(expression::Cstring)::Cint
+ret = @ccall oif.oif_connector_eval_expression(expression::Cstring)::Cint
+check_call(ret)
 
 const N = 2
 A = [2., 0., 1., 0.];
 b = [1., 1.]
 x = [0.,0.]
 ok = @ccall oif.oif_connector_solve(N::Cint, A::Ptr{Cdouble}, b::Ptr{Cdouble}, x::Ptr{Cdouble})::Cint
-if ok != 0
-    println("Cannot load connector")
-    ccall(:jl_exit, Cvoid, (Int32,), ok)
-end
+check_call(ret)
 
 @ccall oif.oif_connector_deinit()::Cvoid
