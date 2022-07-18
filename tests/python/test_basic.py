7329caac7022b162e3f63282723e34f4523c8364
René Fritze <rene.fritze@wwu.de>
2022-07-18 14:09:18 +0200

    [tests] fix python stderr capturing

--- tests/python/test_basic.py
+++ tests/python/test_basic.py
@@ -5,14 +5,14 @@ from typing import Tuple
 import pytest
 
 
-def test_print(oif_lib: Tuple[ctypes.CDLL, str], capsys) -> None:
+def test_print(oif_lib: Tuple[ctypes.CDLL, str], capfd) -> None:
     lib, lang = oif_lib
     err = lib.oif_connector_init(lang.encode())
     assert err == 0
     expression = "print(6*7)".encode()
 
     ret: int = lib.oif_connector_eval_expression(expression)
-    out, err = capsys.readouterr()
+    out, err = capfd.readouterr()
     if ret != 0:
         assert ret == 4  # not implemented
     else:
