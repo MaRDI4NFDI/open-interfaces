78e4b3790f87f334db745e9435183c1317bb6d6e
René Fritze <rene.fritze@wwu.de>
2022-10-10 13:37:23 +0200

    import and call into numpy from C

--- lang_python/oif_python.c
+++ lang_python/oif_python.c
@@ -20,6 +20,8 @@ int oif_lang_init() {
     Py_InitializeEx(0);
     __did_we_init_python = 1;
   }
+  // checks import internally
+  import_array2("Failed to import numpy", OIF_RUNTIME_ERROR);
   return OIF_OK;
 }
 
@@ -36,9 +38,35 @@ int oif_lang_deinit() {
 }
 
 int oif_lang_solve(int N, double *A, double *b, double *x) {
-  (void)N;
-  (void)A;
-  (void)b;
-  (void)x;
-  return OIF_NOT_IMPLEMENTED;
+  long dim_N[1], dim_NN[2];
+  dim_N[0] = N;
+  dim_NN[0] = N;
+  dim_NN[1] = N;
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+  PyObject *np_b = PyArray_SimpleNewFromData(1, dim_N, NPY_DOUBLE, b);
+  PyObject *np_A = PyArray_SimpleNewFromData(2, dim_NN, NPY_DOUBLE, A);
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic pop
+
+  const char *module_name = "numpy.linalg";
+  PyObject *pName = PyUnicode_FromString(module_name);
+  PyObject *pModule = PyImport_Import(pName);
+  Py_DECREF(pName);
+
+  // import function
+  const char *func_name = "solve";
+  PyObject *pFunc = PyObject_GetAttrString(pModule, func_name);
+  PyArrayObject *np_ret =
+      (PyArrayObject *)PyObject_CallFunctionObjArgs(pFunc, np_A, np_b, NULL);
+  Py_DecRef(pFunc);
+  Py_DecRef(pModule);
+  const int ret = OIF_RUNTIME_ERROR ? np_ret == NULL : OIF_OK;
+  if (ret == OIF_OK) {
+    double *x_ret = (double *)PyArray_GETPTR1(np_ret, 0);
+    memcpy(x, x_ret, N * sizeof(double));
+  }
+  Py_DecRef((PyObject *)np_ret);
+  return ret;
 }
