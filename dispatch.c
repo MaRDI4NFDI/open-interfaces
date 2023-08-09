f1526f96ab25725d68d02045826eb3fc3b318792
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-08-09 17:40:32 +0200

    Draft implementation of calling C functions dynamically via FFI

--- dispatch.c
+++ dispatch.c
@@ -2,6 +2,8 @@
 // to the appropriate backend.
 #include "dispatch.h"
 
+#include <dlfcn.h>
+#include <ffi.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -77,9 +79,50 @@ int call_interface_method(
 
 
 int
-run_interface_method_c(const char *method, OIFArgs *args, OIFArgs *retvals) {
-       //void *handle = dlopen("oif_c") 
-       return 0;
+run_interface_method_c(const char *method, OIFArgs *args, OIFArgs *out_args) {
+    void *lib_handle = dlopen("./liboif_backend_c.so", RTLD_LOCAL | RTLD_LAZY); 
+    void *func = dlsym(lib_handle, method);
+
+    fprintf(stderr, "I am definitely here\n");
+
+    int num_args = args->num_args;
+    fprintf(stderr, "num_args = %d\n", num_args);
+    fflush(stderr);
+
+    ffi_cif cif;
+    ffi_type **arg_types = malloc(sizeof(ffi_type) * num_args);
+    fprintf(stderr, "Ready to malloc\n");
+    void **arg_values = malloc(num_args * sizeof(void *));
+    fprintf(stderr, "Malloced\n");
+
+    for (size_t i = 0; i < num_args; ++i) {
+        fprintf(stderr, "In a loop\n");
+        if (args->arg_types[i] == OIF_FLOAT64) {
+            arg_types[i] = &ffi_type_double;
+        } else {
+            fflush(stdout);
+            fprintf(stderr, "Unknown arg type: %d", args->arg_types[i]);
+            fflush(stderr);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (ffi_prep_cif(&cif, FFI_DEFAULT_ABI, num_args, &ffi_type_uint, arg_types) != FFI_OK) {
+        perror("ffi_prep_cif was not OK");
+        exit(EXIT_FAILURE);
+    }
+
+    for (size_t i = 0; i < num_args; ++i) {
+        arg_values[i] = args->args[i];
+    }
+
+    ffi_arg result;
+
+    ffi_call(&cif, FFI_FN(func), &result, arg_values);
+
+    printf("Result is %ld\n", result);
+
+    return 0;
 }
 
 int run_interface_method_python(const char *method, OIFArgs *args, OIFArgs *retvals) {
