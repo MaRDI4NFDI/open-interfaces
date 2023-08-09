d6fecb76a1b4dba265b9f42745093f686660f92c
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-08-09 17:42:56 +0200

    Fix errors: wrong backend name, missing f-string qualifiers

--- examples/qeq_from_c.py
+++ examples/qeq_from_c.py
@@ -1,8 +1,8 @@
 from oif.qeq_solver import QeqSolver
 
-s = QeqSolver("qeq_c")
+s = QeqSolver("c")
 a, b, c = 1.0, 2.0, 1.0
 x = s.solve(a, b, c)
 
-print("Solving quadratic equation for a = {a}, b = {b}, c = {c}")
-print("x = {x}")
+print(f"Solving quadratic equation for a = {a}, b = {b}, c = {c}")
+print(f"x = {x}")
