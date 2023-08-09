7c299ee0ebddd2b34cf306de65b3504ff9e3f7bc
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-08-09 17:43:34 +0200

    Correctly pass backend name

--- src/oif/qeq_solver.py
+++ src/oif/qeq_solver.py
@@ -3,7 +3,7 @@ from oif.core import OIFBackend, init_backend
 
 class QeqSolver:
     def __init__(self, provider: str):
-        self.backend: OIFBackend = init_backend("c", "qeq", 1, 0)
+        self.backend: OIFBackend = init_backend(provider, "qeq", 1, 0)
 
     def solve(self, a: float, b: float, c: float):
         res = self.backend.call("solve", (a, b, c), [])
