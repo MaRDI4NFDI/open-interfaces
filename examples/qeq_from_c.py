fdfbe568ce86aeb3f421243b23a7ae0d238b15c8
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-08-10 11:17:39 +0200

    Move script to function to be able to invoke it from GDB

--- examples/qeq_from_c.py
+++ examples/qeq_from_c.py
@@ -1,8 +1,14 @@
 from oif.qeq_solver import QeqSolver
 
-s = QeqSolver("c")
-a, b, c = 1.0, 2.0, 1.0
-x = s.solve(a, b, c)
 
-print(f"Solving quadratic equation for a = {a}, b = {b}, c = {c}")
-print(f"x = {x}")
+def main():
+    s = QeqSolver("c")
+    a, b, c = 1.0, 2.0, 1.0
+    x = s.solve(a, b, c)
+
+    print(f"Solving quadratic equation for a = {a}, b = {b}, c = {c}")
+    print(f"x = {x}")
+
+
+if __name__ == "__main__":
+    main()
