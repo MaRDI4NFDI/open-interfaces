df9a9a63662915778b56289a12d2589569f90080
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-08-17 15:42:57 +0200

    Add script for calling QeqSolver in Python and the implementation

---
+++ examples/qeq_from_python.py
@@ -0,0 +1,14 @@
+from oif.qeq_solver import QeqSolver
+
+
+def main():
+    s = QeqSolver("python")
+    a, b, c = 1.0, 5.0, 4.0
+    x = s.solve(a, b, c)
+
+    print(f"Solving quadratic equation for a = {a}, b = {b}, c = {c}")
+    print(f"x = {x}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/oif/backend_python/qeq.py
@@ -0,0 +1,36 @@
+import math
+
+import numpy as np
+
+
+def solve_qeq(a: float, b: float, c: float, roots: np.ndarray) -> int:
+    """Solve quadratic equation ax**2 + bx + c = 0.
+
+    Assumes that the output array `roots` always has two elements,
+    so if the roots are repeated, they both will be still present.
+
+    Parameters
+    ----------
+    a, b, c : float
+        Coefficients.
+    roots: np.ndarray[2]
+        Array to which both roots are written.
+
+    Returns
+    -------
+    int
+        Status code.
+    """
+    if a == 0.0:
+        roots[0] = -c / b
+        roots[1] = -c / b
+    else:
+        D = b**2 - 4 * a * c
+        if b > 0:
+            roots[0] = (-b - math.sqrt(D)) / (2 * a)
+            roots[1] = c / (a * roots[0])
+        else:
+            roots[0] = (-b + math.sqrt(D)) / (2 * a)
+            roots[1] = c / (a * roots[0])
+
+    return 0
