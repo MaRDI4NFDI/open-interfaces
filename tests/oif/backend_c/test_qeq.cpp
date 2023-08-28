671b2be920022f9841009c8fea285e74a8dc5026
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-08-28 23:34:16 +0200

    Fix tests for solve_qeq

--- tests/oif/backend_c/test_qeq.cpp
+++ tests/oif/backend_c/test_qeq.cpp
@@ -7,7 +7,7 @@ extern "C" {
 
 TEST(QeqTestSuite, LinearCase) {
     double roots[2];
-    solve_qeq(0.0, 2.0, -1.0, roots);
+    solve_qeq_v1(0.0, 2.0, -1.0, roots);
     EXPECT_EQ(roots[0], 0.5);
     EXPECT_EQ(roots[1], 0.5);
 }
@@ -15,7 +15,7 @@ TEST(QeqTestSuite, LinearCase) {
 
 TEST(QeqTestSuite, TwoRoots) {
     double roots[2];
-    solve_qeq(1.0, 2.0, 1.0, roots);
+    solve_qeq_v1(1.0, 2.0, 1.0, roots);
     EXPECT_EQ(roots[0], -1.0);
     EXPECT_EQ(roots[1], -1.0);
 }
