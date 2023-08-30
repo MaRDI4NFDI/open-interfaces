7c72eeabe82753ce19fd8436f92db2921155826b
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-08-30 18:04:04 +0200

    Extract API into separate header

--- dispatch.h
+++ dispatch.h
@@ -6,32 +6,7 @@
 // Identifier of the used backend.
 typedef size_t BackendHandle;
 
-
-typedef enum {
-    OIF_INT = 1,
-    OIF_FLOAT32 = 2,
-    OIF_FLOAT64 = 3,
-    OIF_FLOAT32_P = 4,
-    OIF_FLOAT64_P = 5,
-    OIF_STR = 6,
-} OIFArgType;
-
-
-typedef struct {
-    size_t num_args;
-    OIFArgType *arg_types;
-    void **arg_values;
-} OIFArgs;
-
-// This structure closely follows PyArray_Object that describes NumPy arrays.
-typedef struct {
-    // Number of dimensions in the array.
-    int nd;
-    // Size of each axis, i = 0, .., nd-1.
-    intptr_t *dimensions;
-    // Pointer to actual data.
-    char *data;
-} OIFArray;
+#include <oif/api.h>
 
 enum
 {
---
+++ include/oif/api.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stddef.h>
+#include <stdint.h>
+
+typedef enum {
+    OIF_INT = 1,
+    OIF_FLOAT32 = 2,
+    OIF_FLOAT64 = 3,
+    OIF_FLOAT32_P = 4,
+    OIF_FLOAT64_P = 5,
+    OIF_STR = 6,
+} OIFArgType;
+
+
+typedef struct {
+    size_t num_args;
+    OIFArgType *arg_types;
+    void **arg_values;
+} OIFArgs;
+
+// This structure closely follows PyArray_Object that describes NumPy arrays.
+typedef struct {
+    // Number of dimensions in the array.
+    int nd;
+    // Size of each axis, i = 0, .., nd-1.
+    intptr_t *dimensions;
+    // Pointer to actual data.
+    char *data;
+} OIFArray;
+
