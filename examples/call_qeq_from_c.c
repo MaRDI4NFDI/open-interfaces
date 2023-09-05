b85569a280b6af21d8048f3618e5e26ff258e10e
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-09-05 11:18:17 +0200

    [examples] Handle errors when calling quadratic solver from C

--- examples/call_qeq_from_c.c
+++ examples/call_qeq_from_c.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <oif/api.h>
 #include <oif/frontend_c/qeq.h>
@@ -15,14 +16,19 @@ int main(int argc, char *argv[])
 
     intptr_t dimensions[] = {2,};
 
-    BackendHandle bh = oif_init_backend("c", "", 1, 0);
+    BackendHandle bh = oif_init_backend("python", "", 1, 0);
+    if (bh == OIF_BACKEND_INIT_ERROR) {
+        fprintf(stderr, "Error during backend initialization. Cannot proceed\n");
+        return EXIT_FAILURE;
+    }
 
     printf("Solving quadratic equation a x^2 + b x + c = 0\n");
     printf("for a = %g, b = %g, c = %g\n", a, b, c);
     OIFArray *roots = create_array_f64(1, dimensions);
     int status = oif_solve_qeq(bh, a, b, c, roots);
     if (status) {
-        fprintf(stderr, "Error occurred\n");
+        free_array(roots);
+        return EXIT_FAILURE;
     }
     printf("x1 = %g\n", ((double *) roots->data)[0]);
     printf("x2 = %g\n", ((double *) roots->data)[1]);
