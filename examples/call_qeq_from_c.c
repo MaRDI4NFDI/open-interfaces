541a01cd8d1882c2e41f21bab2d77f3a58a8a32b
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-09-05 11:40:18 +0200

    [examples] Choose backend for quadratic solver via command-line arguments

--- examples/call_qeq_from_c.c
+++ examples/call_qeq_from_c.c
@@ -1,22 +1,37 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <oif/api.h>
 #include <oif/frontend_c/qeq.h>
 
 
+char *parse_backend(int argc, char *argv[]) {
+    if (argc == 1) {
+        return "c";
+    } else {
+        if ((strcmp(argv[1], "c") == 0) || (strcmp(argv[1], "python") == 0)) {
+            return argv[1];
+        } else {
+            fprintf(stderr, "USAGE: %s [c|python]\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
-    printf("Calling open interfaces from C\n");
+    char *backend = parse_backend(argc, argv);
+    printf("Calling from C an open interface for quadratic solver \n");
+    printf("Backend: %s\n", backend);
 
     double a = 1.0;
     double b = 5.0;
     double c = 4.0;
 
-    intptr_t dimensions[] = {2,};
-
-    BackendHandle bh = oif_init_backend("python", "", 1, 0);
+    BackendHandle bh = oif_init_backend(backend, "", 1, 0);
     if (bh == OIF_BACKEND_INIT_ERROR) {
         fprintf(stderr, "Error during backend initialization. Cannot proceed\n");
         return EXIT_FAILURE;
@@ -24,12 +39,15 @@ int main(int argc, char *argv[])
 
     printf("Solving quadratic equation a x^2 + b x + c = 0\n");
     printf("for a = %g, b = %g, c = %g\n", a, b, c);
+
+    intptr_t dimensions[] = {2,};
     OIFArray *roots = create_array_f64(1, dimensions);
     int status = oif_solve_qeq(bh, a, b, c, roots);
     if (status) {
         free_array(roots);
         return EXIT_FAILURE;
     }
+
     printf("x1 = %g\n", ((double *) roots->data)[0]);
     printf("x2 = %g\n", ((double *) roots->data)[1]);
 
