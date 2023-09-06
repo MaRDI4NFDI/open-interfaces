35a7a7bdc34c09fe7c9bfa2bee4d51c66104d142
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-09-06 11:19:53 +0200

    [examples] Generalize example of calling from Python a quadratic solver
    Now only one script handles both available backends (C and Python)
    via command-line arguments.

--- examples/qeq_from_c.py
+++ examples/qeq_from_c.py
@@ -1,8 +1,23 @@
+import argparse
+
 from oif.qeq_solver import QeqSolver
 
 
+def _parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument("backend", choices=["c", "python"], nargs="?")
+    args = p.parse_args()
+    if args.backend is None:
+        args.backend = "c"
+    return args
+
+
 def main():
-    s = QeqSolver("c")
+    args = _parse_args()
+    backend = args.backend
+    print("Calling from Python an open interface for quadratic solver")
+    print(f"Backend: {backend}")
+    s = QeqSolver(backend)
     a, b, c = 1.0, 5.0, 4.0
     x = s.solve(a, b, c)
 
