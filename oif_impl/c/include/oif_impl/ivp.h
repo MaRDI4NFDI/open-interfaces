612a5096de9c30b7d49358c94ca7ec6297e3e494
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-11-08 21:45:46 +0100

    Add implementation ivp C interface

---
+++ oif_impl/c/include/oif_impl/ivp.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <oif/api.h>
+
+/**
+ * Set right hand side of the system of ordinary differential equations.
+ */
+int oif_ivp_set_rhs_fn(
+    void rhs(double, OIFArrayF64 *y, OIFArrayF64 *y_dot)
+);
+
+/**
+ * Set initial value y(t0) = y0.
+ */
+int oif_ivp_set_initial_value(
+    OIFArrayF64 *y0, double t0
+);
+
+/**
+ * Integrate to time `t` and write the solution to `y`.
+ */
+int oif_ivp_integrate(double t, OIFArrayF64 *y);
