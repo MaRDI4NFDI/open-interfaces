e62264aa219276b2a9262747a8e0fcda9054499a
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2023-11-14 20:16:35 +0100

    Fix bugs in `scipy_ode_dopri5` implementation
    Now there are checks that the function that sets right-hand side
    is invoked before the function that sets initial condition.
    Also, the solver parameters are improved by increasing `nsteps`
    from 500 (default) to 1000

--- oif_impl/impl/ivp/scipy_ode_dopri5/dopri5.py
+++ oif_impl/impl/ivp/scipy_ode_dopri5/dopri5.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy import integrate
 
+_prefix = "scipy_ode_dopri5"
+
 
 class Dopri5:
     def __init__(self):
@@ -9,14 +11,23 @@ class Dopri5:
     def set_rhs_fn(self, rhs):
         self.rhs = rhs
 
+        x = np.array([3.0])
+        assert len(self.rhs(42.0, x)) == len(x)
+        print("after check")
+
         return 0
 
     def set_initial_value(self, y0: np.ndarray, t0: float):
+        _p = f"[{_prefix}::set_initial_value]"
+
         if not isinstance(t0, float):
-            raise ValueError("Argument `t0` must be floating-point number")
+            raise ValueError(f"{_p} Argument `t0` must be floating-point number")
+
+        if self.rhs is None:
+            raise TypeError(f"{_p} Method `set_rhs_fn` must be called earlier")
 
         self.s = integrate.ode(self.rhs).set_integrator(
-            "dopri5", atol=1e-15, rtol=1e-15
+            "dopri5", atol=1e-15, rtol=1e-15, nsteps=1000
         )
         self.s.set_initial_value(y0, t0)
 
