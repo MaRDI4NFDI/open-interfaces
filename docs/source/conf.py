5ed869d82fd975610959f981fa3199d989f6d5cf
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-01-25 18:28:45 +0100

    Update Sphinx configuration file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,24 +6,31 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "oif-toy-example"
-copyright = "2023, Dmitry I. Kabanov"
-author = "Dmitry I. Kabanov"
+project = "MaRDI Open Interfaces"
+copyright = "2023--2024 MaRDI Open Interfaces authors"
 release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 numfig = True
+math_numfig = True
+numfig_secnum_depth = 2
+math_eqref_format = "Eq. {number}"  # Space after . is non-breaking space!!!
 
-extensions = ["myst_parser"]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.mathjax",
+]
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns = ["build"]
 
+myst_enable_extensions = ["dollarmath", "amsmath"]
+myst_dmath_double_inline = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "furo"
 html_static_path = ["_static"]
