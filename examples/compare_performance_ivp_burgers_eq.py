2510b3007e2c16b786d0218da09e81480974f6e8
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-01-30 19:05:59 +0100

    Increase the list of resolutions for performance study

--- examples/compare_performance_ivp_burgers_eq.py
+++ examples/compare_performance_ivp_burgers_eq.py
@@ -9,7 +9,7 @@ from oif.interfaces.ivp import IVP
 from scipy import integrate
 
 IMPL_LIST = ["scipy_ode_dopri5", "sundials_cvode", "native_scipy_ode_dopri5"]
-RESOLUTIONS = [101, 1001, 10_001]
+RESOLUTIONS = [101, 1001, 2001, 4001, 8001, 10_001]
 
 RESULT_SOLUTION_FILENAME_TPL = os.path.join("assets", "ivp_burgers_soln_{}.pdf")
 RESULT_PERF_FILENAME = os.path.join("assets", "ivp_burgers_perf.pdf")
