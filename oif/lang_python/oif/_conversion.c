a6641fdc5a51f44dbe1d6f45984ce9070078a2e1
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-02-08 17:52:17 +0100

    Add draft version of _conversion Python C extension

---
+++ oif/lang_python/oif/_conversion.c
@@ -0,0 +1,77 @@
+#define PY_SSIZE_T_CLEAN
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include <Python.h>
+#include <ffi.h>
+#include <numpy/arrayobject.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "oif/api.h"
+
+static PyObject *
+numpy_array_from_oif_array_f64(PyObject *Py_UNUSED(self), PyObject *args) {
+    PyObject *retval = NULL;
+    void *ctypes_pointer;
+
+    // We need to initialize PyArray_API (table of function pointers)
+    // in every translation unit (separate .c file).
+    // See the details in the accepted solution here:
+    // https://stackoverflow.com/q/47026900/1095202
+    if (PyArray_API == NULL) {
+        import_array();
+    }
+    OIFArrayF64 *arr;
+
+    PyObject_Print(args, stdout, 0);
+    fprintf(stdout, "\n");
+
+    if (!PyArg_ParseTuple(args, "w*", &arr)) {
+        fprintf(stderr, "[_conversion] Could not parse function arguments\n");
+        return NULL;
+    }
+
+    printf("arr->nd = %d\n", arr->nd);
+    printf("arr->dimensions = %ld\n", arr->dimensions[0]);
+
+    retval = PyArray_SimpleNewFromData(
+        arr->nd, arr->dimensions, NPY_FLOAT64, arr->data);
+
+    if (retval == NULL) {
+        fprintf(stderr,
+                "[_conversion] Could not create a new NumPy array\n");
+    }
+
+    return retval;
+}
+
+static PyMethodDef callback_methods[] = {
+    {"numpy_array_from_oif_array_f64",
+     numpy_array_from_oif_array_f64,
+     METH_VARARGS,
+     "Invoke a given C function from Python."},
+    {NULL, NULL, 0, NULL} /* Sentinel */
+};
+
+PyDoc_STRVAR(
+    conversion_doc,
+    "Conversion functions between C and Python types");
+
+static struct PyModuleDef callbackmodule = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "_conversion",  /* name of module */
+    .m_doc = conversion_doc, /* module documentation, may be NULL */
+    .m_size = -1,          /* size of per-interpreter state of the module,
+                              or -1 if the module keeps state in global variables. */
+    .m_methods = callback_methods};
+
+PyMODINIT_FUNC PyInit__conversion(void) {
+    PyObject *m;
+
+    m = PyModule_Create(&callbackmodule);
+    if (m == NULL) {
+        return NULL;
+    }
+
+    return m;
+}
+
