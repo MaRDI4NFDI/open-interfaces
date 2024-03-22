29f9d50f6c3b069d8ea19581528c036e79830e3f
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-03-22 17:08:21 +0100

    Move languages enumeration to `api.h`

--- oif/include/oif/api.h
+++ oif/include/oif/api.h
@@ -20,6 +20,15 @@ typedef enum {
     OIF_CALLBACK = 7,
 } OIFArgType;
 
+enum {
+    OIF_LANG_C = 1,
+    OIF_LANG_CXX = 2,
+    OIF_LANG_PYTHON = 3,
+    OIF_LANG_JULIA = 4,
+    OIF_LANG_R = 5,
+    OIF_LANG_COUNT = 6,
+};
+
 typedef struct {
     size_t num_args;
     OIFArgType *arg_types;
--- oif/include/oif/dispatch_api.h
+++ oif/include/oif/dispatch_api.h
@@ -4,15 +4,6 @@
  */
 #include <oif/api.h>
 
-enum {
-    OIF_LANG_C = 1,
-    OIF_LANG_CXX = 2,
-    OIF_LANG_PYTHON = 3,
-    OIF_LANG_JULIA = 4,
-    OIF_LANG_R = 5,
-    OIF_LANG_COUNT = 6,
-};
-
 // Identifier for the language-specific dispatch library (C, Python, etc.).
 typedef unsigned int DispatchHandle;
 
