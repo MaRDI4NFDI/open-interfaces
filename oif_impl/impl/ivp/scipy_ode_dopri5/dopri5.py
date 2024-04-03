3110b92e1865bc1635ba37a0627f2086f217c86f
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-04-03 17:24:18 +0200

    Force method invocation order for scipy_ode_dopri5 implementation

--- oif_impl/impl/ivp/scipy_ode_dopri5/dopri5.py
+++ oif_impl/impl/ivp/scipy_ode_dopri5/dopri5.py
@@ -44,6 +44,8 @@ class Dopri5(IVPInterface):
         return 0
 
     def set_tolerances(self, rtol, atol):
+        if self.s is None:
+            raise RuntimeError("`set_rhs_fn` must be called before `set_tolerances`")
         self.s.set_integrator("dopri5", rtol=rtol, atol=atol, nsteps=1000)
         if hasattr(self, "y0"):
             self.s.set_initial_value(self.y0, self.t0)
