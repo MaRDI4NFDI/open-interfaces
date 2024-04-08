41c2a05f24e72e21a952b8c89a5cc9aa77ef62bf
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-04-08 17:45:46 +0200

    Update attempt to call Julia functions from C

--- oif_impl/lang_julia/dispatch_julia.c
+++ oif_impl/lang_julia/dispatch_julia.c
@@ -25,6 +25,29 @@ load_impl(const char *impl_details, size_t version_major, size_t version_minor)
 
     static_assert(sizeof(int) == 4, "The code is written in assumption that C int is 32-bit");
 
+    const char *module_filename = "oif_impl/impl/qeq/jl_qeq_solver/qeq_solver.jl";
+    /* const char *module_name = "Main.QeqSolver"; */
+    char include_statement[512];
+    sprintf(include_statement, "include(\"%s\")", module_filename);
+    jl_eval_string(include_statement);
+    jl_eval_string("using .QeqSolver");
+    /* jl_load(module, module_name); */
+    /* jl_value_t * mod = (jl_value_t*)jl_eval_string("Solvers"); */
+    /* jl_function_t * func = jl_get_function((jl_module_t*)module, "solve"); */
+
+    int64_t (*func_jl)(double, double, double, double *) = jl_unbox_voidpointer(jl_eval_string("@cfunction(QeqSolver.solve!, Int, (Float64, Float64, Float64, Ref{Float64}))"));
+    if (func_jl == NULL) {
+        fprintf(
+            stderr,
+            "[%s] We could not obtain a pointer to C-compatible Julia function\n",
+            prefix_
+        );
+    }
+    double roots[2] = {99.0, 25.0};
+    int ret = (int) func_jl(1.0, 5.0, 4.0, roots);
+    fprintf(stderr, "[%s] We called QeqSolver.solve\n", prefix_);
+    fprintf(stderr, "roots1 = %f, roots2 = %f\n", roots[0], roots[1]);
+
     result = malloc(sizeof *result);
     if (result == NULL) {
         fprintf(stderr,
