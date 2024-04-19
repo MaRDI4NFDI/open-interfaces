136a71fb1d159d6be35b392708540b7e633bccd8
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-04-19 16:11:30 +0200

    Make IVP implementation in Julia object-oriented

--- oif_impl/impl/ivp/jl_diffeq/jl_diffeq.jl
+++ oif_impl/impl/ivp/jl_diffeq/jl_diffeq.jl
@@ -1,28 +1,59 @@
 module JlDiffEq
-export set_initial_value, set_rhs_fn, set_tolerances, integrate, set_user_data, callback, OIFArrayF64
+export Self, set_initial_value, set_rhs_fn, set_tolerances, integrate, set_user_data
 
-function set_initial_value(y0, t0)
+using DifferentialEquations: ODEProblem, init, step!, Tsit5
+
+mutable struct Self
+    t0::Float64
+    y0::Vector{Float64}
+    problem::ODEProblem
+    s
+    user_data
+    function Self()
+        t0 = 5.0
+        return new(t0, [])
+    end
+end
+
+function set_initial_value(self::Self, y0, t0)
     println("I am setting initial value")
+    self.t0 = t0
+    self.y0 = y0
+    println("Initial time is = ", self.t0)
     return 0
 end
 
-function set_rhs_fn(rhs)::Int
+function set_rhs_fn(self::Self, rhs)::Int
     println("I am setting rhs") 
-    return 0
+    self.rhs = rhs
+
+    if y0
+        self.problem = ODEProblem(rhs_wrapper(rhs), y0, ())
+        self.s = integrator(prob, Tsit5())
+    end
 end
 
-function set_tolerances(rtol::Float64, atol::Float64)::Int
+function set_tolerances(self::Self, rtol::Float64, atol::Float64)::Int
     println("I am setting tolerances")
     return 0
 end
 
-function integrate!(t::Float64, y::Vector{Float64})::Int
+function integrate(self::Self, t::Float64, y::Vector{Float64})::Int
     println("I am integrating")
+    step!(self.integrator, t - self.integrator.t, true)
+    y[:] = self.integrator.u
     return 0
 end
 
-function set_user_data(user_data)::Int
+function set_user_data(self::Self, user_data)::Int
     println("I am setting user data")
+end
+
+function rhs_wrapper(rhs)
+    function wrapper(du, u, p, t)
+        return rhs(t, u, udot, p)
+    end
+    return wrapper
 end
 
 end
