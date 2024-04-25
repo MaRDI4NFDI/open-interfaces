ea80d06ad449c9b4d1e295e6984c7e3a216f95ec
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-04-25 17:39:52 +0200

    Work on making Julia OrdinaryDiffEq implementation work

--- oif_impl/impl/ivp/jl_diffeq/jl_diffeq.jl
+++ oif_impl/impl/ivp/jl_diffeq/jl_diffeq.jl
@@ -1,18 +1,17 @@
 module JlDiffEq
-# export Self, set_initial_value, set_rhs_fn, set_tolerances, integrate, set_user_data
+export Self, set_initial_value, set_rhs_fn, set_tolerances, integrate, set_user_data
 
 using OrdinaryDiffEq: ODEProblem, Tsit5, init
 
 mutable struct Self
     t0::Float64
     y0::Vector{Float64}
-    problem
     rhs
+    problem
     s
-    user_data
+    user_data::Tuple
     function Self()
-        t0 = 5.0
-        return new(t0, [])
+        return new(0.0, [])
     end
 end
 
@@ -24,31 +23,46 @@ function set_initial_value(self::Self, y0, t0)
     return 0
 end
 
-function set_rhs_fn(self::Self, rhs)::Int
-    println("I am setting rhs") 
+function set_rhs_fn(self::Self, rhs)
     self.rhs = rhs
 
     if !isempty(self.y0)
-        self.problem = ODEProblem(rhs_wrapper(rhs), self.y0, (self.t0, Inf), ())
+        if !isdefined(self, :user_data)
+            println("I am here")
+            self.problem = ODEProblem(
+                rhs_wrapper(rhs), self.y0, (self.t0, Inf)
+            )
+        else
+            println("I am there")
+            self.problem = ODEProblem(
+                rhs_wrapper(rhs), self.y0, (self.t0, Inf), self.user_data
+            )
+        end
+        println("Do I come here?")
+        println(self.problem)
         self.s = init(self.problem, Tsit5())
+    else
+        throw(MethodError("Method `set_initial_value` must be called before `set_rhs_fn`"))
     end
+    println("End of set_rhs")
 end
 
-# function set_tolerances(self::Self, rtol::Float64, atol::Float64)::Int
-#     println("I am setting tolerances")
-#     return 0
-# end
+function set_tolerances(self::Self, rtol::Float64, atol::Float64)::Int
+    println("I am setting tolerances")
+    return 0
+end
 
-# function integrate(self::Self, t::Float64, y::Vector{Float64})::Int
-#     println("I am integrating")
-#     step!(self.integrator, t - self.integrator.t, true)
-#     y[:] = self.integrator.u
-#     return 0
-# end
+function integrate(self::Self, t::Float64, y::Vector{Float64})::Int
+    println("I am integrating")
+    step!(self.integrator, t - self.integrator.t, true)
+    y[:] = self.integrator.u
+    return 0
+end
 
-# function set_user_data(self::Self, user_data)::Int
-#     println("I am setting user data")
-# end
+function set_user_data(self::Self, user_data)::Int
+    println("I am setting user data")
+    self.user_data = user_data
+end
 
 function rhs_wrapper(rhs)
     function wrapper(du, u, p, t)
