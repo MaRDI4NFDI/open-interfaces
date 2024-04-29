2bc82b16c4b96e7bacb2f64a93c712f7dbc727f4
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-04-29 12:17:45 +0200

    Extract function for converting Julia arrays to pointers to OIFArrayF64

--- oif_impl/lang_julia/callback.jl
+++ oif_impl/lang_julia/callback.jl
@@ -10,17 +10,21 @@ function make_wrapper_over_c_callback(fn_c::Ptr{Cvoid})::Function
         if typeof(user_data) == SciMLBase.NullParameters
             user_data = C_NULL
         end
-        dimensions = Base.unsafe_convert(Ptr{Clong}, collect(size(y)))
-        data = Base.unsafe_convert(Ptr{Float64}, y)
-        oif_y = Ref(OIFArrayF64(ndims(y), dimensions, data))
 
-        dimensions = Base.unsafe_convert(Ptr{Clong}, collect(size(ydot)))
-        data = Base.unsafe_convert(Ptr{Float64}, ydot)
-        oif_ydot = Ref(OIFArrayF64(ndims(ydot), dimensions, data))
+        oif_y = _oif_array_f64_pointer_from_array_f64(y)
+        oif_ydot = _oif_array_f64_pointer_from_array_f64(ydot)
 
         @ccall $fn_c(t::Float64, oif_y::Ptr{OIFArrayF64}, oif_ydot::Ptr{OIFArrayF64}, user_data::Ptr{Cvoid})::Cint
         return 0
     end
     return wrapper
+end
+
+function _oif_array_f64_pointer_from_array_f64(arr::AbstractArray{T, N}) where {T<:Float64, N}
+    ndim = ndims(arr)
+    dimensions = Base.unsafe_convert(Ptr{Clong}, collect(size(arr)))
+    data = Base.unsafe_convert(Ptr{Float64}, arr)
+    oif_arr = Ref(OIFArrayF64(ndim, dimensions, data))
+    return oif_arr
 end
 end
