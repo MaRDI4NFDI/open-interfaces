6dd54026ea40175ad7c421807a5e4b112d4cc6be
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-06-18 17:14:03 +0200

    Implement serialization and deserialization

--- tests/lang_c/test_config_dict.cpp
+++ tests/lang_c/test_config_dict.cpp
@@ -40,3 +40,22 @@ TEST(OIFConfigDictTest, GetValues)
 
     oif_config_dict_free(dict);
 }
+
+TEST(OIFConfigDictTest, Serialize)
+{
+    OIFConfigDict *dict = oif_config_dict_init();
+
+    oif_config_dict_add_int(dict, "int_option", 42);
+    oif_config_dict_add_int(dict, "int_neg_option", -12345);
+    oif_config_dict_add_double(dict, "double_option", 2.718);
+
+    oif_config_dict_serialize(dict);
+    OIFConfigDict *new_dict = oif_config_dict_deserialize(dict);
+
+    ASSERT_EQ(oif_config_dict_get_int(new_dict, "int_option"), 42);
+    ASSERT_EQ(oif_config_dict_get_int(new_dict, "int_neg_option"), -12345);
+    ASSERT_EQ(oif_config_dict_get_double(new_dict, "double_option"), 2.718);
+
+    oif_config_dict_free(dict);
+    oif_config_dict_free(new_dict);
+}
