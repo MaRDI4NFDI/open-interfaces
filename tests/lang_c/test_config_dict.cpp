3b91c085d7c8741318e6f59dd792fe71a258ad06
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-06-19 17:21:49 +0200

    Add test to check that serialization for a large buffer can occur

--- tests/lang_c/test_config_dict.cpp
+++ tests/lang_c/test_config_dict.cpp
@@ -41,7 +41,7 @@ TEST(OIFConfigDictTest, GetValues)
     oif_config_dict_free(dict);
 }
 
-TEST(OIFConfigDictTest, Serialize)
+TEST(OIFConfigDictTest, SerializeDeserializeBasicCase)
 {
     OIFConfigDict *dict = oif_config_dict_init();
 
@@ -55,6 +55,48 @@ TEST(OIFConfigDictTest, Serialize)
     ASSERT_EQ(oif_config_dict_get_int(new_dict, "int_option"), 42);
     ASSERT_EQ(oif_config_dict_get_int(new_dict, "int_neg_option"), -12345);
     ASSERT_EQ(oif_config_dict_get_double(new_dict, "double_option"), 2.718);
+
+    oif_config_dict_free(dict);
+    oif_config_dict_free(new_dict);
+}
+
+TEST(OIFConfigDictTest, SerializeDeserializeVeryLongStringCase)
+{
+    OIFConfigDict *dict = oif_config_dict_init();
+
+    const int N = 20;
+
+    char buffer[1024];
+    for (size_t i = 0; i < N; ++i) {
+        sprintf(buffer, "int_option_%zu", i);
+        oif_config_dict_add_int(dict, buffer, i);
+    }
+    for (size_t i = 0; i < N; ++i) {
+        sprintf(buffer, "int_neg_option_%zu", i);
+        oif_config_dict_add_int(dict, buffer, -i);
+    }
+    for (size_t i = 0; i < N; ++i) {
+        sprintf(buffer, "double_option_%.6f", 3.14*i);
+        oif_config_dict_add_double(dict, buffer, 3.14*i);
+    }
+
+    oif_config_dict_serialize(dict);
+    OIFConfigDict *new_dict = oif_config_dict_deserialize(dict);
+
+    for (size_t i = 0; i < N; ++i) {
+        sprintf(buffer, "int_option_%zu", i);
+        ASSERT_EQ(oif_config_dict_get_int(new_dict, buffer), i);
+    }
+
+    for (size_t i = 0; i < N; ++i) {
+        sprintf(buffer, "int_neg_option_%zu", i);
+        ASSERT_EQ(oif_config_dict_get_int(new_dict, buffer), -i);
+    }
+
+    for (size_t i = 0; i < N; ++i) {
+        sprintf(buffer, "double_option_%.6f", 3.14 * i);
+        ASSERT_EQ(oif_config_dict_get_double(new_dict, buffer), 3.14 * i);
+    }
 
     oif_config_dict_free(dict);
     oif_config_dict_free(new_dict);
