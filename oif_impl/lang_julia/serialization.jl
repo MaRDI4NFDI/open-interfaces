77cb02474bb8d8d244f5d4a240a06e6106acd35e
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-07-16 17:37:06 +0200

    Correctly deserialize OIF_CONFIG_DICT in Julia

--- oif_impl/lang_julia/serialization.jl
+++ oif_impl/lang_julia/serialization.jl
@@ -3,10 +3,23 @@ export deserialize
 import MsgPack
 
 function deserialize(sd::Ptr)::Dict
-    sd_str = unsafe_string(Ptr{Cchar}(sd))
+    # We get unsigned bytes and it is important to keep them this way
+    # to get correct conversion.
+    sd_str = unsafe_string(Ptr{UInt8}(sd))
     io = IOBuffer(sd_str)
 
-    data = MsgPack.unpack(io)
+    data = []
+    i = 1
+    while !eof(io)
+        elem = MsgPack.unpack(io)
+        if i % 2 == 1
+            push!(data, Symbol(elem))
+        else
+            push!(data, elem)
+        end
+        i += 2
+    end
+
     @assert length(data) % 2 == 0
 
     resultant_dict = Dict()
