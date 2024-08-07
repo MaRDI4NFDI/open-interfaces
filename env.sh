ce035ec6e6bf04a4a9b671c571618de83a0e99c0
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2024-08-07 15:38:54 +0200

    Add env.sh to configure environment

---
+++ env.sh
@@ -0,0 +1,26 @@
+# Set project root directory to use absolute paths below.
+export PRJ_ROOT_DIR="$PWD"
+
+# Set path to implementations
+export OIF_IMPL_ROOT_DIR="${PRJ_ROOT_DIR}"
+
+# Add compiled libraries to the path for the linker.
+if [ -n "$LD_LIBRARY_PATH" ]; then
+    export _OLD_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
+fi
+
+export LD_LIBRARY_PATH="$PRJ_ROOT_DIR/build":"$LD_LIBRARY_PATH"
+
+# Find Python packages inside the `src` directory.
+if [ -n "$PYTHONPATH" ]; then
+    export _OLD_PYTHONPATH="$PYTHONPATH"
+fi
+
+oifpath=${PRJ_ROOT_DIR}/oif/interfaces/python
+oifpath="$oifpath":${PRJ_ROOT_DIR}/oif/lang_python
+oifpath="$oifpath":${PRJ_ROOT_DIR}/oif_impl/lang_python
+oifpath="$oifpath":${PRJ_ROOT_DIR}/oif_impl/impl
+oifpath="$oifpath":${PRJ_ROOT_DIR}/build
+export PYTHONPATH="$oifpath":"$PYTHONPATH"
+
+export JULIA_PROJECT="$PRJ_ROOT_DIR"
