d9e850afacacbf06d989ef0d888aa81fad92270e
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-01-21 14:30:06 +0100

    [ivp scipy_ode] Apply all set integrator parameters when setting tolerances
    When user calls `set_tolerances` method on the `ivp::scipy_ode`
    implementation, then the integration parameters were errorneously
    reset to default values except `rtol` and `atol`.
    
    This commit fixes this problem, by applying all set integrator
    parameters from the `integrator_params` dictionary.

--- oif_impl/impl/ivp/scipy_ode/scipy_ode.py
+++ oif_impl/impl/ivp/scipy_ode/scipy_ode.py
@@ -44,9 +44,12 @@ class ScipyODE(IVPInterface):
     def set_tolerances(self, rtol, atol):
         if self.s is None:
             raise RuntimeError("`set_rhs_fn` must be called before `set_tolerances`")
-        self.s.set_integrator(self.integrator, rtol=rtol, atol=atol)
-        self.rtol = rtol
-        self.atol = atol
+
+        self.integrator_params = self.integrator_params | {
+            "rtol": rtol,
+            "atol": atol,
+        }
+        self.s.set_integrator(self.integrator, **self.integrator_params)
         if hasattr(self, "y0"):
             self.s.set_initial_value(self.y0, self.t0)
         return 0
