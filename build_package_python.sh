ef0a7f7b111c1d7939e11492145360fcfec96838
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-04-25 18:49:37 +0200

    Improve build_python_package shell script

--- build_package_python.sh
+++ build_package_python.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
-rm -rf dist/oif-0.5.0/
-
+package="openinterfaces"
+version=$(grep 'version =' pyproject.toml | sed 's/version = "//' | sed 's/"//')
+echo -e "\e[01;32mBuilding package: $package version $version\e[0m"
+rm -r "dist/${package}-${version}/" && echo "deleted old dist/${package}-${version}/"
 
 if ! python -m build --sdist ; then
     echo -e "\e[01;31mERROR: build failed\e[0m"
     exit 1
 fi
 
-tar xzf dist/oif-0.5.0.tar.gz -C dist/
-open dist/oif-0.5.0/
+tar xzf "dist/${package}-${version}.tar.gz" -C dist/
+open "dist/${package}-${version}/"
