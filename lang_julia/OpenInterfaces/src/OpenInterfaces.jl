011a31779f4721bb8a272b4ff21bdc50f2f13cdc
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-05-13 17:25:37 +0200

    [julia] Expand definition of integer constants and data structure to match what api.h declares

--- lang_julia/OpenInterfaces/src/OpenInterfaces.jl
+++ lang_julia/OpenInterfaces/src/OpenInterfaces.jl
@@ -2,8 +2,17 @@ module OpenInterfaces
 # Define constants and data structures equivalent to the C interface.
 # See oif/api.h
 
-export OIFArgType, OIFArgs, OIFArrayF64
+# Type ids
 export OIF_INT, OIF_FLOAT64, OIF_ARRAY_F64, OIF_STR, OIF_CALLBACK, OIF_USER_DATA, OIF_CONFIG_DICT
+
+# Language ids
+export OIF_LANG_C, OIF_LANG_CXX, OIF_LANG_PYTHON, OIF_LANG_JULIA, OIF_LANG_R
+
+# Error codes
+export OIF_ERROR, OIF_IMPL_INIT_ERROR
+
+# Data structures
+export OIFArgs, OIFArrayF64, OIFCallback, OIFUserData
 
 @enum OIFArgType begin
     OIF_INT = 1
@@ -17,16 +26,42 @@ export OIF_INT, OIF_FLOAT64, OIF_ARRAY_F64, OIF_STR, OIF_CALLBACK, OIF_USER_DATA
     OIF_CONFIG_DICT = 9
 end
 
-struct OIFArrayF64
-    nd::Int32
-    dimensions::Ptr{Int64}
-    data::Ptr{Float64}
+@enum OIFLang begin
+    OIF_LANG_C = 1
+    OIF_LANG_CXX = 2
+    OIF_LANG_PYTHON = 3
+    OIF_LANG_JULIA = 4
+    OIF_LANG_R = 5
+    OIF_LANG_COUNT = 6
+end
+
+@enum OIFError begin
+    OIF_ERROR = -1
+    OIF_IMPL_INIT_ERROR = -2
 end
 
 struct OIFArgs
     num_args::Int64
     arg_types::Ptr{OIFArgType}
     arg_values::Ptr{Ptr{Cvoid}}
+end
+
+struct OIFArrayF64
+    nd::Int32
+    dimensions::Ptr{Int64}
+    data::Ptr{Float64}
+end
+
+struct OIFCallback
+    src::Int32
+    fn_p_py::Ptr{Cvoid}
+    fn_p_c::Ptr{Cvoid}
+end
+
+struct OIFUserData
+    src::Int32
+    c::Ptr{Cvoid}
+    py::Ptr{Cvoid}
 end
 
 end # module OpenInterfaces
