732782e0ee7e3b8f685e267e43b8cc8993035929
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-05-27 15:21:58 +0200

    [python] Log a message about Fortran array in NumPyLinsolve

--- lang_python/oif_impl/openinterfaces/_impl/linsolve/numpy/linsolve.py
+++ lang_python/oif_impl/openinterfaces/_impl/linsolve/numpy/linsolve.py
@@ -4,4 +4,6 @@ from openinterfaces._impl.interfaces.linsolve import LinsolveInterface
 
 class NumPyLinsolve(LinsolveInterface):
     def solve_lin(self, A, b, result):
+        if np.isfortran(A):
+            print("[linsolve::numpy] Receive matrix in Fortran-order")
         result[:] = np.linalg.solve(A, b)
