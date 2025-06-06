1a018b885e9e6186b5a6ed503a18941664459512
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-06-06 18:00:02 +0200

    [julia] Add WIP on rewriting tests for IVP with multiple problems

--- tests/lang_julia/test_ivp.jl
+++ tests/lang_julia/test_ivp.jl
@@ -7,6 +7,25 @@ using OpenInterfaces.Interfaces.IVP
 IMPLEMENTATIONS = ["sundials_cvode"]
 
 
+mutable struct ScalarExpDecayProblem
+    t0::Float64 = 0.0
+    y0::Vector{Float64}
+    rhs::Function
+    exact::Function
+    ScalarExpDecayProblem()
+        t0 = 0.0
+        y0 = [1.0]
+        function rhs(t, y, ydot, user_data)
+            ydot[1] = -y[1]
+            return 0
+        end
+        exact = (t) -> y0[1] * exp(-t)
+
+        new(t0, y0, rhs, exact)
+    end
+end
+
+
 function rhs_fn(t, y, ydot, user_data)::Int32
     ydot[1], ydot[2] = -y[1], -y[2]  # Example RHS function
     return 0
@@ -16,39 +35,29 @@ end
 
     function test(testCore)
         for impl in IMPLEMENTATIONS
-            println("Testing impl $impl")
-            impl_self = IVP.Self(impl)
-            println("Testing implementation implh =", impl_self.implh)
-            testCore(impl_self)
+            for p in PROBLEMS
+                println("Testing impl $impl")
+                println("Test problem $p")
+                impl_self = IVP.Self(impl)
+                println("Testing implementation implh =", impl_self.implh)
+                testCore(impl_self, p)
+            end
         end
     end
 
     @testset "test_1" begin
-        test() do impl_self
-            y0 = [1.0, 2.0]
-            t0 = 0.0
-            rtol = 1e-6
-            atol = 1e-12
-            user_data = "example_data"
-            integrator_name = "RK4"
-            integrator_params = Dict("step_size" => 0.1)
+        test() do impl_self, p
+            @test IVP.set_initial_value(impl_self, p.y0, p.t0) == 0
+            @test impl_self.N == length(p.y0)
+            @test IVP.set_rhs_fn(impl_self, p.rhs) == 0
 
-            @test IVP.set_initial_value(impl_self, y0, t0) == 0
-            @test impl_self.N == length(y0)
-            @test IVP.set_rhs_fn(impl_self, rhs_fn) == 0
-            @test IVP.set_tolerances(impl_self, rtol, atol) == 0
-            # IVP.set_user_data(impl_self, user_data)
-            # IVP.set_integrator(impl_self, integrator_name, integrator_params)
-
-            t_final = 1.0
-            times = collect(range(0, t_final, 11))
+            t1 = p.t0 + 1
+            times = collect(range(p.t0, t1, 11))
             for t in times[2:end]
-                IVP.integrate(impl_self, t_final)
+                IVP.integrate(impl_self, t)
             end
 
-            @test impl_self.y ≈ [exp(-t_final), 2*exp(-t_final)] rtol=1e-6 atol=1e-8
-
-            # IVP.print_stats(impl_self)
+            @test impl_self.y[end] ≈ p.exact(t1) rtol=2e-4
         end
     end
 
