6b4f0122990390859da39f11200454b3fd17f11b
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-06-06 17:40:49 +0200

    [julia] Fix errors in type conversion code from OIF types to Julia types

--- include/oif/api.h
+++ include/oif/api.h
@@ -64,8 +64,9 @@ typedef struct {
 // This structure is used for callback functions.
 typedef struct {
     int src;        // Language of the function (one of OIF_LANG_* constants)
-    void *fn_p_py;  // Function pointer in Python
     void *fn_p_c;   // Function pointer in C
+    void *fn_p_jl;  // Function pointer in Julia
+    void *fn_p_py;  // Function pointer in Python
 } OIFCallback;
 
 /**
