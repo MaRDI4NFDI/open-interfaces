e1dd038f742dfc3ef9fbeb28bd797c3d5f9387e9
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-06-06 17:42:02 +0200

    [julia] Add first non-trivial passing test for IVP gateway

--- tests/lang_julia/test_ivp.jl
+++ tests/lang_julia/test_ivp.jl
@@ -6,6 +6,12 @@ using OpenInterfaces.Interfaces.IVP
 # IMPLEMENTATIONS = ["c_lapack", "numpy", "jl_backslash"]
 IMPLEMENTATIONS = ["sundials_cvode"]
 
+
+function rhs_fn(t, y, ydot, user_data)::Int32
+    ydot[1], ydot[2] = -y[1], -y[2]  # Example RHS function
+    return 0
+end
+
 @testset "Testing IVP interface from Julia" begin
 
     function test(testCore)
@@ -21,24 +27,26 @@ IMPLEMENTATIONS = ["sundials_cvode"]
         test() do impl_self
             y0 = [1.0, 2.0]
             t0 = 0.0
-            rhs_fn = (t, y) -> [-y[1], -y[2]]  # Example RHS function
             rtol = 1e-6
             atol = 1e-12
             user_data = "example_data"
             integrator_name = "RK4"
             integrator_params = Dict("step_size" => 0.1)
 
-            IVP.set_initial_value(impl_self, y0, t0)
+            @test IVP.set_initial_value(impl_self, y0, t0) == 0
             @test impl_self.N == length(y0)
-            IVP.set_rhs_fn(impl_self, rhs_fn)
-            IVP.set_tolerances(impl_self, rtol, atol)
+            @test IVP.set_rhs_fn(impl_self, rhs_fn) == 0
+            @test IVP.set_tolerances(impl_self, rtol, atol) == 0
             # IVP.set_user_data(impl_self, user_data)
             # IVP.set_integrator(impl_self, integrator_name, integrator_params)
 
-            # t_final = 10.0
-            # IVP.integrate(impl_self, t_final)
+            t_final = 1.0
+            times = collect(range(0, t_final, 11))
+            for t in times[2:end]
+                IVP.integrate(impl_self, t_final)
+            end
 
-            # @test impl_self.y ≈ [exp(-t_final), exp(-t_final)] rtol=1e-6 atol=1e-6
+            @test impl_self.y ≈ [exp(-t_final), 2*exp(-t_final)] rtol=1e-6 atol=1e-8
 
             # IVP.print_stats(impl_self)
         end
