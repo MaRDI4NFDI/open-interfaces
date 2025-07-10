a46014ce0f7e60a945fee6e0a723c0c5426b12b0
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-07-10 17:24:15 +0200

    [c, util] Add useful macros to test for used sanitizers

--- include/oif/_platform.h
+++ include/oif/_platform.h
@@ -11,4 +11,18 @@ extern "C" {
 
 #ifdef __cplusplus
 }
+#if defined(__SANITIZE_ADDRESS__)
+    #define OIF_SANITIZE_ADDRESS_ENABLED 1
 #endif
+
+#if defined(__clang__) || defined (__GNUC__)
+# define OIF_ATTRIBUTE_NO_SANITIZE_ADDRESS __attribute__((no_sanitize_address))
+#else
+# define OIF_ATTRIBUTE_NO_SANITIZE_ADDRESS
+#endif
+// Usage example:
+// OIF_ATTRIBUTE_NO_SANITIZE_ADDRESS
+// void my_function()  {
+//     ...Function implementation...
+// }
+
