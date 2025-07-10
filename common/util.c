eb971b8f1920c93a819eab954fee698578699b59
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-07-10 17:16:46 +0200

    [c, util] Implement wrappers around `malloc` and `free` to fight with memory leaks

--- common/util.c
+++ common/util.c
@@ -1,4 +1,6 @@
+#define _GNU_SOURCE
 #include <ctype.h>
+#include <dlfcn.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -6,6 +8,94 @@
 #include <stdlib.h>
 
 #include <oif/util.h>
+
+static size_t NALLOCS_ = 0;
+static size_t NBYTES_ = 0;
+
+void *oif_util_malloc_(size_t nbytes) {
+    // void *addr = __builtin_return_address(0);
+    // Dl_info info;
+    // if (dladdr(addr, &info) && info.dli_fname) {
+    //     fprintf(
+    //         stderr,
+    //         "\033[31m"
+    //         "[oif_util_malloc]"
+    //         "\033[0m"
+    //         " Allocating %zu bytes of memory (total allocs: %zu)\n"
+    //         "                  File: %s\n"
+    //         "              Function: %s\n"
+    //         "                  Line: %d\n",
+    //         nbytes, NALLOCS_ + 1, info.dli_fname, info.dli_sname, __LINE__
+    //     );
+    // } else {
+    //     fprintf(stderr, "[oif_util_malloc] Could not retrieve caller information\n");
+    // }
+
+    // void *p_raw = malloc(nbytes + sizeof(size_t));
+    // if (p_raw == NULL) {
+    //     fprintf(stderr, "Could not allocate memory\n");
+    //     exit(1);
+    // }
+    // NALLOCS_++;
+    // NBYTES_ += nbytes;
+
+    // size_t *p2 = (size_t *)p_raw;
+    // *p2 = nbytes;
+    // void *p_oif = (void *)(p2 + 1);
+    // fprintf(stderr, "Pointer malloc: %p\n", p_raw);
+    // fprintf(stderr, "    Size in it: %zu bytes\n", nbytes);
+    // fprintf(stderr, "   Pointer oif: %p\n", p_oif);
+    //
+    void *p_oif = malloc(nbytes);
+    NALLOCS_++;
+
+    return p_oif;
+}
+
+void *oif_util_malloc_verbose(size_t nbytes, const char *file, const char *func, int line) {
+    fprintf(
+        stderr,
+        "\033[31m"
+        "[oif_util_malloc]"
+        "\033[0m"
+        " Allocating %zu bytes of memory\n"
+        "                  File: %s\n"
+        "              Function: %s\n"
+        "                  Line: %d\n",
+        nbytes, file, func, line
+    );
+    return oif_util_malloc_(nbytes);
+}
+
+void oif_util_free_(void *ptr) {
+    if (ptr == NULL) {
+        fprintf(stderr, "Cannot free a NULL pointer\n");
+        exit(1);
+    }
+
+    size_t *p_oif = (size_t *)ptr;
+    // size_t *p_raw = p_oif - 1;
+    // size_t nbytes = *p_raw;
+    // fprintf(stderr, "Pointer to free: %p\n", ptr);
+    // fprintf(stderr, "    Pointer raw: %p\n", p_raw);
+    // fprintf(stderr, "     Size in it: %zu bytes\n", nbytes);
+    free(p_oif);
+    p_oif = NULL;
+    NALLOCS_--;
+    // NBYTES_ -= nbytes;
+}
+
+void oif_util_free_verbose(void *ptr, const char *file, const char *func, int line) {
+    fprintf(
+        stderr,
+        "\033[31m[oif_util_free]\033[0m Freeing memory\n"
+        "                  File: %s\n"
+        "              Function: %s\n"
+        "                  Line: %d\n",
+        file, func, line
+    );
+    oif_util_free_(ptr);
+}
 
 uint32_t
 oif_util_u32_from_size_t(size_t val)
@@ -26,8 +116,23 @@ oif_util_u32_from_size_t(size_t val)
 char *
 oif_util_str_duplicate(const char *src)
 {
+    void *addr = __builtin_return_address(0);
+    Dl_info info;
+    if (dladdr(addr, &info) && info.dli_fname) {
+        fprintf(
+            stderr,
+            "\033[31m[oif_util_str_duplicate]\033[0m\n"
+            "        File: %s\n"
+            "    Function: %s\n"
+            "        Line: %d\n",
+            info.dli_fname,
+            info.dli_sname,
+            __LINE__);
+    } else {
+        fprintf(stderr, "[str_duplicate] Could not retrieve caller information\n");
+    }
     size_t len = strlen(src);
-    char *dest = malloc((len + 1) * sizeof(*dest));
+    char *dest = oif_util_malloc_((len + 1) * sizeof(*dest));
     if (dest == NULL) {
         fprintf(stderr, "[str_duplicate] Could not allocate memory\n");
         return NULL;
@@ -69,3 +174,15 @@ logerr(const char *prefix, const char *fmt, ...)
 
     return 0;
 }
+
+
+#if defined(__GNUC__)
+#if !defined(__OPTIMIZE__)
+void __attribute((destructor))
+oif_util_dtor_(void) {
+    fprintf(stderr, "[oif_util] Final statistics on memory allocs via malloc/free:\n");
+    fprintf(stderr, "[oif_util] Number of not-freed allocations: %zu\n", NALLOCS_);
+    // fprintf(stderr, "[oif_util] Number of not-freed bytes: %zu\n", NBYTES_);
+}
+#endif
+#endif
