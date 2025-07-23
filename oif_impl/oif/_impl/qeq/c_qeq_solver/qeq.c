dcd01994731e155ef5ecf1e73a28679b33b1a608
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-07-23 17:36:31 +0200

    [c] Add OOP to QEQ and Linsolve C implementations
    Two C implementations for the QEQ and Linsolve interfaces
    now have as first argument a dummy structure `Self *self`,
    due to the use of object-oriented features.
    However, as these interfaces have pure functions,
    these `Self` structures are empty.

--- oif_impl/oif/_impl/qeq/c_qeq_solver/qeq.c
+++ oif_impl/oif/_impl/qeq/c_qeq_solver/qeq.c
@@ -6,9 +6,15 @@
 
 #include "_qeq.c"
 
+// We do not actually need to store any data in the `Self` structure,
+// hence it is an empty object.
+typedef struct self {
+} Self;
+
 int
-solve_qeq(double a, double b, double c, OIFArrayF64 *roots)
+solve_qeq(Self *self, double a, double b, double c, OIFArrayF64 *roots)
 {
+    (void)self;
     int status = solve_qeq_v1(a, b, c, roots->data);
     return status;
 }
