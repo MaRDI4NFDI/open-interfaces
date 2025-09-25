eeb91d4d78ea2192af671f0add95e94f60182dc0
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-09-25 14:00:13 +0200

    [examples] Update C and Python examples (new implementations, logic, memory leaks)

--- examples/call_linsolve_from_python.py
+++ examples/call_linsolve_from_python.py
@@ -13,7 +13,11 @@ class Args:
 def _parse_args():
     p = argparse.ArgumentParser()
     p.add_argument(
-        "impl", type=str, choices=["c_lapack", "numpy"], default="c_lapack", nargs="?"
+        "impl",
+        type=str,
+        choices=["c_lapack", "jl_backslash", "numpy"],
+        default="c_lapack",
+        nargs="?",
     )
     args = p.parse_args()
     return Args(**vars(args))
