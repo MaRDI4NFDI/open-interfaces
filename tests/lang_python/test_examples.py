0a63e80b03ab14f3a96fba7896233d37dd0a3c17
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-11-11 16:49:31 +0100

    [tests, python] Add tests for VdP example

--- tests/lang_python/test_examples.py
+++ tests/lang_python/test_examples.py
@@ -51,4 +51,44 @@ def test_call_ivp_from_python_burgers_eq__exit_success(impl_name: str):
 
 
 # -----------------------------------------------------------------------------
-# def test_call_ivp_from_python_vdp__dopri5__fails(impl_name: str):
+# Van der Pol equation: unsuccessful and successful runs.
+
+
+@pytest.mark.parametrize(
+    "unsuccessful_impl",
+    [
+        "scipy_ode-dopri5",
+        "scipy_ode-dopri5-100k",
+        "scipy_ode-vode",
+    ],
+)
+def test_call_ivp_from_python_vdp__dopri5__fails(unsuccessful_impl: str):
+    p = subprocess.run(
+        ["python", "examples/call_ivp_from_python_vdp.py", unsuccessful_impl],
+        capture_output=True,
+        text=True,
+    )
+    assert p.returncode != 0
+    assert "RuntimeError: Error occurred while executing method 'integrate'" in p.stderr
+
+
+@pytest.mark.parametrize(
+    "successful_impl",
+    [
+        "scipy_ode-vode-40k",
+        # "sundials_cvode-default",  # It is simply too slow to run (40 secs)
+        "jl_diffeq-rosenbrock23",
+    ],
+)
+def test_call_ivp_from_python_vdp__with_successful_impl_succeeds(successful_impl: str):
+    p = subprocess.run(
+        [
+            "python",
+            "examples/call_ivp_from_python_vdp.py",
+            "--do-not-save-anything",
+            successful_impl,
+        ],
+        capture_output=True,
+        text=True,
+    )
+    assert p.returncode == 0
