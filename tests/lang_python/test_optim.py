8214579ee541f9106435e25f82f84ff06be19650
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2025-12-19 15:49:12 +0100

    [tests, python] Update tests for `optim` interface

--- tests/lang_python/test_optim.py
+++ tests/lang_python/test_optim.py
@@ -5,8 +5,14 @@ from openinterfaces.interfaces.optim import Optim
 
 # -----------------------------------------------------------------------------
 # Problems
-def objective_fn(x):
+def convex_objective_fn(x):
     return np.sum(x**2)
+
+
+def rosenbrock_objective_fn(x):
+    """The Rosenbrock function with additional arguments"""
+    a, b = (0.5, 1.0)
+    return sum(a * (x[1:] - x[:-1] ** 2.0) ** 2.0 + (1 - x[:-1]) ** 2.0) + b
 
 
 @pytest.fixture(params=["scipy_optimize"])
@@ -20,9 +26,25 @@ def test__simple_convex_problem__converges(s):
     x0 = np.array([0.5, 0.6, 0.7])
 
     s.set_initial_guess(x0)
-    s.set_objective_fn(objective_fn)
+    s.set_objective_fn(convex_objective_fn)
 
-    result = s.minimize()
+    status, message = s.minimize()
+    x = s.x
 
-    assert result.status == 0
-    assert np.all(np.abs(result.x) < 1e-6)
+    assert status == 0
+    assert len(x) == len(x0)
+    assert np.all(np.abs(x) < 1e-6)
+
+
+def test__rosenbrock_problem__converges(s):
+    x0 = np.array([3.14, 2.72, 42.0, 9.81, 8.31])
+
+    s.set_initial_guess(x0)
+    s.set_objective_fn(rosenbrock_objective_fn)
+
+    status, message = s.minimize()
+    x = s.x
+
+    assert status == 0
+    assert len(x) == len(x0)
+    assert np.all(np.abs(x - 1.0) < 1e-5)  # The solution is [1, 1, ..., 1].
