f36a587f8bed08df9f9bde40082bb99371d6583a
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2026-02-26 18:45:30 +0100

    [julia, optim] Rename jloptim to jl_optim

--- lang_julia/_impl/optim/jl_optim/jl_optim.jl
+++ lang_julia/_impl/optim/jl_optim/jl_optim.jl
@@ -10,13 +10,17 @@ using Ipopt
 
 mutable struct Self
     x0::Vector{Float64}
+    objective_fn::Function
+    user_data::Any
     function Self()
         return new()
     end
 end
 
 function set_initial_guess(self::Self, x0)
+    println("=== debug == x0 = ", x0)
     self.x0 = x0
+    return nothing
 end
 
 function set_user_data(self::Self, user_data)
@@ -24,19 +28,32 @@ function set_user_data(self::Self, user_data)
 end
 
 function set_objective_fn(self::Self, objective_fn)
+    println("=== debug === objective_fn = ", objective_fn)
     self.objective_fn = objective_fn
 end
 
 function set_method(self, method_name, method_params)
-    throw RuntimeError()
+    throw(MethodError())
 end
 
 function minimize(self::Self, out_x)::Tuple{Int, String}
-    model = Model(Ipopt.optimizer)
+    model = Model(Ipopt.Optimizer)
     @variable(model, x, start = self.x0)
     @objective(model, Min, self.objective_fn)
     optimize!(model)
 
+    jump_status = termination_status(model)
+    if jump_status == OPTIMAL
+        status = 0
+    else
+        status = -1
+    end
+
+    if jump_status == OPTIMAL
+        message = "Optimization was successful"
+    else
+        message = "Optimization has failed"
+    end
 
     return (status, message)
 end
