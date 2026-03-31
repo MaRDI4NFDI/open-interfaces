4b090d01645a0a5198d1a174e52cc47573e8f4a2
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2026-03-31 16:05:23 +0200

    [julia, ipopt_jl] Implement set_method with checks on allowed parameters

--- lang_julia/_impl/optim/ipopt_jl/ipopt_jl.jl
+++ lang_julia/_impl/optim/ipopt_jl/ipopt_jl.jl
@@ -12,7 +12,7 @@ mutable struct Self
     x0::Vector{Float64}
     objective_fn::Union{Function,Nothing}
     user_data::Any
-    options::Dict
+    method_params::Dict
     function Self()
         return new([], nothing, nothing, Dict())
     end
@@ -33,23 +33,44 @@ end
 
 function set_method(self, method_name, method_params)
     println("[ipopt_jl] `method_name` is not supported by this implementation")
-    self.options = merge(self.options, method_params)
+
+    # To check that we have valid parameters,
+    # we use a simple model and optimize a convex problem on it.
+    # This is needed, as the actual checks on the parameters
+    # is done in `optimize!` in `Ipopt.jl`.
+    guinea_pig = Model(Ipopt.Optimizer)
+    set_silent(guinea_pig)
+    @variable(guinea_pig, x_[1:2])
+    @objective(guinea_pig, Min, x_[1]^2 + x_[2]^2)
+    for (key, value) in method_params
+        attr = String(key)
+        set_attribute(guinea_pig, attr, value)
+    end
+    optimize!(guinea_pig)
+
+    # If we reach this point, this means that passed `method_params`
+    # are allowed parameters.
+    self.method_params = merge(self.method_params, method_params)
 end
 
 function minimize(self::Self, out_x)::Tuple{Int,String}
     if !isdefined(self, :user_data)
         self.user_data = nothing
     end
-    model = Model(Ipopt.Optimizer)
     N = length(self.x0)
-    @variable(model, x[1:N])
+    model = Model(Ipopt.Optimizer)
+    x = @variable(model, [1:N])
 
-    for (key, value) in self.options
-        set_attribute(model, key, value)
-    end
     set_start_value.(x, self.x0)
     wrapper(x) = self.objective_fn(x, self.user_data)
     @objective(model, Min, wrapper(x))
+
+    # Set options (`method_params`) that were passed earlier in `set_method`.
+    for (key, value) in self.method_params
+        attr = String(key)
+        set_attribute(model, attr, value)
+    end
+
     optimize!(model)
     out_x .= value.(x)
 
