bcfe85bccf401b6d7682704921c8b3d0861eb6ef
Dmitry Kabanov <dmitry.kabanov@uni-muenster.de>
2026-05-05 18:09:10 +0200

    [misc] Reformat source code

--- tests/lang_julia/aux_funcs.c
+++ tests/lang_julia/aux_funcs.c
@@ -1,29 +1,37 @@
 #include <oif/api.h>
 
-
 typedef struct context {
     int p1;
     double p2;
 } Context;
 
-
-int add_two_ints(int a, int b) {
+int
+add_two_ints(int a, int b)
+{
     return a + b;
 }
 
-double add_two_doubles(double a, double b) {
+double
+add_two_doubles(double a, double b)
+{
     return a + b;
 }
 
-int add_i32_f64__ret_i32(int a, double b) {
-    return (int) (a + b);
+int
+add_i32_f64__ret_i32(int a, double b)
+{
+    return (int)(a + b);
 }
 
-double add_i32_f64__ret_f64(int a, double b) {
+double
+add_i32_f64__ret_f64(int a, double b)
+{
     return a + b;
 }
 
-int axpy_f64_arrf64_arrf64__ret_i32(double alpha, OIFArrayF64 *x, OIFArrayF64 *y) {
+int
+axpy_f64_arrf64_arrf64__ret_i32(double alpha, OIFArrayF64 *x, OIFArrayF64 *y)
+{
     int N = x->dimensions[0];
     for (int i = 0; i < N; i++) {
         y->data[i] = y->data[i] + alpha * x->data[i];
@@ -32,7 +40,10 @@ int axpy_f64_arrf64_arrf64__ret_i32(double alpha, OIFArrayF64 *x, OIFArrayF64 *y
     return 0;
 }
 
-int axpy_with_context_f64_arrf64_arrf64__ret_i32(double alpha, OIFArrayF64 *x, OIFArrayF64 *y, void *void_context) {
+int
+axpy_with_context_f64_arrf64_arrf64__ret_i32(double alpha, OIFArrayF64 *x, OIFArrayF64 *y,
+                                             void *void_context)
+{
     Context *context = void_context;
     int N = x->dimensions[0];
     for (int i = 0; i < N; i++) {
